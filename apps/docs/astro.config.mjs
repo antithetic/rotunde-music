5ddff5b4c0a7403dd552b5c4031ee47d8732c933
Raúl Garcia <ego.war@icloud.com>
2026-01-17 02:20:35 -0800

    ✨ Docs sitemap
    add sitemap to docs, update robots.txt

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -5,42 +5,43 @@ import starlight from '@astrojs/starlight'
 import starlightThemeFlexoki from 'starlight-theme-flexoki'
 import starlightDocSearch from '@astrojs/starlight-docsearch'
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: SITE_NAME,
-			// social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Project Overview',
-					slug: 'index',
-				},
-				{
-					label: 'Sanity',
-					autogenerate: { directory: 'sanity' },
-				},
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-			plugins: [starlightThemeFlexoki({
-				accentColor: "yellow",
-			}),
-			starlightDocSearch({
-				appId: '8YI0O6D19I',
-				apiKey: '5a420334ea36fb52762b984e072829df',
-				indexName: 'Rotunde Docs',
-			}),
-			],
-		}),
-	],
+    site: 'https://rotunde-docs.vercel.app/',
+    integrations: [starlight({
+        title: SITE_NAME,
+        // social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+        sidebar: [
+            {
+                label: 'Project Overview',
+                slug: 'index',
+            },
+            {
+                label: 'Sanity',
+                autogenerate: { directory: 'sanity' },
+            },
+            {
+                label: 'Guides',
+                items: [
+                    // Each item here is one entry in the navigation menu.
+                    { label: 'Example Guide', slug: 'guides/example' },
+                ],
+            },
+            {
+                label: 'Reference',
+                autogenerate: { directory: 'reference' },
+            },
+        ],
+        plugins: [starlightThemeFlexoki({
+            accentColor: "yellow",
+        }),
+        starlightDocSearch({
+            appId: '8YI0O6D19I',
+            apiKey: '5a420334ea36fb52762b984e072829df',
+            indexName: 'Rotunde Docs',
+        }),
+        ],
+		}), sitemap()],
 });

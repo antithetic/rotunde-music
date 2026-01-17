f419a9f1f2a36a459b3660c255f1afdc3e71354e
Raúl Garcia <ego.war@icloud.com>
2026-01-17 02:08:26 -0800

    ✨ Docs Search
    upgrade docs search to algolia

--- apps/docs/astro.config.mjs
+++ apps/docs/astro.config.mjs
@@ -1,15 +1,16 @@
 // @ts-check
-import { SITE_NAME, SITE_URL } from '@repo/constants';
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { SITE_NAME, SITE_URL } from '@repo/constants'
+import { defineConfig } from 'astro/config'
+import starlight from '@astrojs/starlight'
 import starlightThemeFlexoki from 'starlight-theme-flexoki'
+import starlightDocSearch from '@astrojs/starlight-docsearch'
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
 			title: SITE_NAME,
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			// social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
 			sidebar: [
 				{
 					label: 'Project Overview',
@@ -33,8 +34,13 @@ export default defineConfig({
 			],
 			plugins: [starlightThemeFlexoki({
 				accentColor: "yellow",
-			}
-			)],
+			}),
+			starlightDocSearch({
+				appId: '8YI0O6D19I',
+				apiKey: '5a420334ea36fb52762b984e072829df',
+				indexName: 'Rotunde Docs',
+			}),
+			],
 		}),
 	],
 });

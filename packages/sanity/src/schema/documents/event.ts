95ffe40a903eccf83c7682d38e6f74f7faecb840
Raúl Garcia <ego.war@icloud.com>
2026-01-17 23:56:20 -0800

    🎨 Event schema validation
    updated validation for events

--- packages/sanity/src/schema/documents/event.ts
+++ packages/sanity/src/schema/documents/event.ts
@@ -11,5 +11,51 @@ export const event = defineType({
       name: 'name',
       type: 'string',
     }),
+    defineField({
+      name: 'slug',
+      type: 'slug',
+      options: {source: 'name'},
+      validation: (rule) =>
+        rule.required().error(`Required to generate a page on the website`),
+    }),
+    defineField({
+      name: 'eventType',
+      type: 'string',
+      options: {
+        list: ['in-person', 'virtual'],
+        layout: 'radio',
+      },
+    }),
+    defineField({
+      name: 'date',
+      type: 'datetime',
+    }),
+    defineField({
+      name: 'doorsOpen',
+      type: 'number',
+    }),
+    defineField({
+      name: 'venue',
+      type: 'reference',
+      to: [{type: 'venue'}],
+    }),
+    defineField({
+      name: 'headline',
+      type: 'reference',
+      to: [{type: 'artist'}],
+    }),
+    defineField({
+      name: 'image',
+      type: 'image',
+    }),
+    defineField({
+      name: 'details',
+      type: 'array',
+      of: [{type: 'block'}],
+    }),
+    defineField({
+      name: 'tickets',
+      type: 'url',
+    }),
   ],
 })

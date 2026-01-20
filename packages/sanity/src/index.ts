8ebc965b1bdcb69a642760b9691833a5af220620
Raúl Garcia <ego.war@icloud.com>
2026-01-20 02:54:44 -0800

    🎨 References structure
    adds a reference count badge to all documents showing how many other documents reference them.

--- packages/sanity/src/index.ts
+++ packages/sanity/src/index.ts
@@ -3,6 +3,7 @@ import {visionTool} from '@sanity/vision'
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {media} from 'sanity-plugin-media'
+import {references, referencesView} from 'sanity-plugin-references'
 import {singletonTools} from 'sanity-plugin-singleton-management'
 import {tags} from 'sanity-plugin-tags-v4'
 import {userSelect} from 'sanity-plugin-user-select-input'
@@ -17,12 +18,16 @@ export default defineConfig({
   dataset: 'production',
 
   plugins: [
-    structureTool(),
+    structureTool({
+      defaultDocumentNode: (S) =>
+        S.document().views([S.view.form(), referencesView(S)]),
+    }),
     visionTool(),
     media(),
     singletonTools(),
     tags(),
     userSelect(),
+    references(),
   ],
 
   schema: {

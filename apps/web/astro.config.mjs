80f83a1bacb4e70218845a0503ae4dc5ecb3b1ec
Raúl Garcia <ego.war@icloud.com>
2026-02-02 02:26:39 -0800

    ✨ Tailwind
    add tailwind css for styling

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -3,8 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import vercel from '@astrojs/vercel';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   adapter: vercel(),
   output: 'static',
+
+  vite: {
+    // @ts-ignore
+    plugins: [tailwindcss()],
+  },
 });

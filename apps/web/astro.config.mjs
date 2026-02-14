8baff77a385e428b27ceb78cce98e61fb906d99a
Raúl Garcia <ego.war@icloud.com>
2026-02-13 22:17:29 -0800

    ✨ Programme Component
    A program component for displaying upcoming events and community gatherings.

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -6,15 +6,16 @@ import vercel from '@astrojs/vercel';
 import UnoCSS from 'unocss/astro'
 
 
+import react from '@astrojs/react';
+
+
 // https://astro.build/config
 export default defineConfig({
   adapter: vercel(),
   output: 'static',
-  integrations: [
-    UnoCSS({
-      // injectReset: true // or a path to the reset file
-    }),
-  ],
+  integrations: [UnoCSS({
+    // injectReset: true // or a path to the reset file
+  }), react()],
   vite: {
     // @ts-ignore
     plugins: [],
